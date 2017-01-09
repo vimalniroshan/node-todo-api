d7fb61b6c624069f4b6995552c97d46d46cb19bc
vimalniroshan <vimalniroshan@users.noreply.github.com>
2017-01-08 21:27:06 -0800

    Setup separate test and dev envs

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+var env = process.env.NODE_ENV || 'dev';
+
+if(env === 'dev') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
