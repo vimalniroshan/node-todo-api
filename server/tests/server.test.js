5539c4ee55d5701b72b4e5eff68f10a9e94436e9
vimalniroshan <vimalniroshan@users.noreply.github.com>
2017-01-21 16:22:16 -0800

    Add DELETE /user/me/token tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -334,3 +334,22 @@ describe('POST /user/login', () => {
       });
   });
 });
+
+describe('DELETE /user/me/token', () => {
+  it('should delete auth token on logout', (done) => {
+    request(app)
+      .delete('/user/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+})
